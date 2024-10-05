39a971a62c84180cd676f55cd3c5e8f5d03a6a2a
danny <dsetpjang99@gmail.com>
2024-10-05 12:01:22 +0800

    完成建立事項的網頁與程式碼建置

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 
@@ -12,3 +13,40 @@ def todolist(request):
 
     print(todos)
     return render(request, "todo/todolist.html", {"todos": todos})
+
+
+def todo(request, id):
+    message = ""
+    user = request.user
+    todo = ""
+    try:
+        todo = Todo.objects.get(id=id, user=user)
+    except Exception as e:
+        print(e)
+        message = "編號錯誤"
+
+    return render(request, "todo/todo.html", {"todo": todo, "message": message})
+
+
+def create_todo(request):
+    message = ""
+    user = request.user
+    form = None
+    if not user.is_authenticated:
+        message = "請先登入"
+    else:
+        form = TodoForm()
+        if request.method == "POST":
+            try:
+                print(request.POST)
+                form = TodoForm(request.POST)
+                todo = form.save(commit=False)
+                todo.user = request.user
+                todo.save()
+                message = "提交成功!"
+                return redirect("todolist")
+            except Exception as e:
+                print(e)
+                message = "提交失敗!"
+
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
